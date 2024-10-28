c483b7464c8f8359e787187a5692a6a7532be13e
unknown <satyadanielramadhan15@gmail.com>
2024-10-28 22:03:09 +0700

    Searching

--- phpmvc/app/core/Constants.php
+++ phpmvc/app/core/Constants.php
@@ -1,3 +1,3 @@
 <?php   
 
-define('BASEURL',  'http://localhost/Prakweb_2024_C_22304011/PHPMVC/phpmvc/public');
+define('BASEURL',  'http://localhost/Prakweb_2024_C_223040011/PHPMVC/phpmvc/Public/');
