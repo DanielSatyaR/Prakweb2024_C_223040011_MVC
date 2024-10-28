abb874980b2e96b4ae1ba08f99ab055eaea23b52
unknown <satyadanielramadhan15@gmail.com>
2024-10-28 20:59:50 +0700

    Searching

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php   
+
+define('BASEURL',  'http://localhost/prakweb2024_c_22304011/PHPMVC/phpmvc/public');
--- phpmvc/app/core/Controller.php
+++ phpmvc/app/core/Controller.php
@@ -1,17 +1,14 @@
 <?php
 
-class Controller
-{
-    // controller view methodnya
-    public function view($view, $data = [])
-    {
-        require_once '../app/views/' . $view . '.php';
+class Controller {
+    public function view($view, $data = []) {
+        // Menggunakan __DIR__ untuk mendapatkan jalur absolut
+        require_once __DIR__ . '/../views/' . $view . '.php';
     }
 
-
-    public function model($model)
-    {
-        require_once '../app/models/' . $model . '.php';
-        return new $model;
+    public function model($model) {
+        // Menggunakan __DIR__ untuk jalur absolut
+        require_once __DIR__ . '/../models/' . $model . '.php';
+        return new $model();
     }
 }
